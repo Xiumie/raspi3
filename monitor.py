3b9fc94ca86cfee79c93d7a9c49db6d0367dc986
Xiu <Xiumie@users.noreply.github.com>
2017-07-19 13:33:02 +0800

    Update monitor.py
    优化结构

--- monitor.py
+++ monitor.py
@@ -7,12 +7,14 @@ import sys, os
 import subprocess
 
 class Monitor(object):
-    def __init__(self, updown_pin=20, rightleft_pin=21, ud_angle=60, rl_angle=10):
+    def __init__(self, ud_angle=60, rl_angle=10, updown_pin=20, rightleft_pin=21):
         # 初始化配置
         self.__updown_pin = updown_pin
         self.__rightleft_pin = rightleft_pin
         self.__ud_angle = ud_angle
         self.__rl_angle = rl_angle
+        # up down right left 步进幅度
+        self.__adjust = 5
         
         # GPIO.cleanup()
         # 设置GPIO模式&&忽略警报
@@ -57,42 +59,42 @@ class Monitor(object):
     
     def up(self):
         # 当角度值不超范围时 运动
-        if 0 <= self.__ud_angle < 180:
-            self.__ud_angle = self.__ud_angle - 10
+        if 0 < self.__ud_angle <= 180:
+            self.__ud_angle = self.__ud_angle - self.__adjust
         else:
             return
         self.__ud.ChangeDutyCycle(2.5 + 10*self.__ud_angle/180)
-        time.sleep(0.02)
+        time.sleep(0.1)
         self.__ud.ChangeDutyCycle(0)
         time.sleep(0.1)
         
     def down(self):
         if 0 <= self.__ud_angle < 180:
-            self.__ud_angle = self.__ud_angle + 10
+            self.__ud_angle = self.__ud_angle + self.__adjust
         else:
             return
         self.__ud.ChangeDutyCycle(2.5 + 10*self.__ud_angle/180)
-        time.sleep(0.02)
+        time.sleep(0.1)
         self.__ud.ChangeDutyCycle(0)
         time.sleep(0.1)
         
     def right(self):
-        if 0 <= self.__rl_angle < 180:
-            self.__rl_angle = self.__rl_angle - 10
+        if 0 < self.__rl_angle <= 180:
+            self.__rl_angle = self.__rl_angle - self.__adjust
         else:
             return
         self.__rl.ChangeDutyCycle(2.5 + 10*self.__rl_angle/180)
-        time.sleep(0.02)
+        time.sleep(0.1)
         self.__rl.ChangeDutyCycle(0)
         time.sleep(0.1)
         
     def left(self):
-        if 0 < self.__rl_angle <= 180:
-            self.__rl_angle = self.__rl_angle + 10
+        if 0 <= self.__rl_angle < 180:
+            self.__rl_angle = self.__rl_angle + self.__adjust
         else:
             return
         self.__rl.ChangeDutyCycle(2.5 + 10*self.__rl_angle/180)
-        time.sleep(0.02)
+        time.sleep(0.1)
         self.__rl.ChangeDutyCycle(0)
         time.sleep(0.1)
         
@@ -101,31 +103,57 @@ if __name__ == '__main__':
     try:
         while True:
             os.chdir("/var/www/html/img")
-            cmd = 'sudo raspistill -o 1.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            clouldmon.set_angle(60,90)
-            cmd = 'sudo raspistill -o 2.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            clouldmon.set_angle(60,170)
-            cmd = 'sudo raspistill -o 3.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            print "ok"
-            time.sleep(60*10)
-            
-            os.chdir("/var/www/html/img")
-            cmd = 'sudo raspistill -o 3.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            clouldmon.set_angle(60,90)
-            cmd = 'sudo raspistill -o 2.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            clouldmon.set_angle(60,10)
-            cmd = 'sudo raspistill -o 1.jpg -t 1000 -rot 180'
-            pid = subprocess.call(cmd, shell=True)
-            print "ok"
-            time.sleep(60*10)
+            # 时间判断 08:00:00-20:00:00
+            if 80000 < int(time.strftime("%H%M%S")) < 200000:
+                # 调用shell拍照
+                cmd = 'sudo raspistill -o main.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                # 调用shell使用scp传送到远程服务器
+                # cmd = 'scp -P 23 filepath username@ip:filepath'
+                # pid = subprocess.call(cmd, shell=True)
+                print time.strftime("%H:%M:%S")+" ok"
+                time.sleep(60*10)
+            else:
+                cmd = 'sudo raspistill -o 1.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,45)
+                cmd = 'sudo raspistill -o 2.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,90)
+                cmd = 'sudo raspistill -o 3.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,135)
+                cmd = 'sudo raspistill -o 4.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,170)
+                cmd = 'sudo raspistill -o 5.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+
+                print time.strftime("%H:%M:%S")+" ok"
+                time.sleep(60*10)
+                
+                cmd = 'sudo raspistill -o 5.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,135)
+                cmd = 'sudo raspistill -o 4.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,90)
+                cmd = 'sudo raspistill -o 3.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,45)
+                cmd = 'sudo raspistill -o 2.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                clouldmon.set_angle(60,10)
+                cmd = 'sudo raspistill -o 1.jpg -t 1000 -rot 180'
+                pid = subprocess.call(cmd, shell=True)
+                
+                print time.strftime("%H:%M:%S")+" ok"
+                time.sleep(60*10)
             
     except Exception, e:
         clouldmon.stop()
         print str(e)
+        
+        
         
 
